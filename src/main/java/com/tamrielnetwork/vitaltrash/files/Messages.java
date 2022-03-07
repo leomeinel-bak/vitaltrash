acf107ad92289c9c1dcc1139cf32cbadb9961f3a
TamrielNetwork <contact@tamriel.me>
2022-03-07 05:58:07 +0100

    Update Copyright to feature contributors

--- src/main/java/com/tamrielnetwork/vitaltrash/files/Messages.java
+++ src/main/java/com/tamrielnetwork/vitaltrash/files/Messages.java
@@ -1,6 +1,6 @@
 /*
  * VitalTrash is a Spigot Plugin that gives players the ability to open a trash interface.
- * Copyright © 2022 Leopold Meinel
+ * Copyright © 2022 Leopold Meinel & contributors
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
